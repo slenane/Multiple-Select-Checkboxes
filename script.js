3f58388830109cee643fd5fa3623a33d62bb37db
Stephen Lenane <51973143+slenane@users.noreply.github.com>
2020-08-11 19:36:02 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+let checkboxes = document.querySelectorAll(".inbox input[type='checkbox']");
+
+let lastChecked;
+
+function handleCheck(e) {
+    //Check if they have the shift key down AND they are checking it.
+    let inBetween = false;
+    if (e.shiftKey && this.checked) {
+        //loop over every checkbox
+        checkboxes.forEach(checkbox => {
+            if (checkbox === this || checkbox === lastChecked) {
+                inBetween = !inBetween;
+            }
+
+            if(inBetween) {
+                checkbox.checked = true;
+            }
+        });
+    }
+    
+    lastChecked = this;
+}
+
+checkboxes.forEach(checkbox => 
+    checkbox.addEventListener("click", handleCheck));
